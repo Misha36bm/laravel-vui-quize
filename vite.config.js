30273ec8999b26d24ed18e607439a17a6aca7833
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2025-06-11 21:04:46 +0300

    Revert "vuejs init"
    This reverts commit 671f4f503fb654f5ce66b83c92f3dbcffa625852.

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,13 @@
-import { defineConfig, loadEnv } from "vite";
-import laravel from "laravel-vite-plugin";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), "");
-
-    return {
-        server:
-            env.APP_ENV === "local"
-                ? {
-                      host: "0.0.0.0",
-                      port: 5173,
-                      strictPort: true,
-                      hmr: {
-                          host: "quize.loc",
-                      },
-                  }
-                : undefined,
-        plugins: [
-            laravel({
-                input: "resources/js/app.js",
-                refresh: true,
-            }),
-            vue({
-                template: {
-                    transformAssetUrls: {
-                        base: null,
-                        includeAbsolute: false,
-                    },
-                },
-            }),
-        ],
-    };
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
 });
