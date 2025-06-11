fd68734062a45d1d736d4b067794083407ad5a16
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2025-06-11 21:20:43 +0300

    install frontend dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,42 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, loadEnv } from "vite";
+import laravel from "laravel-vite-plugin";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+export default defineConfig(() => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        server:
+            env.APP_ENV === "local"
+                ? {
+                      host: "0.0.0.0",
+                      port: 5173,
+                      strictPort: true,
+                      hmr: {
+                          host: "quize.loc",
+                      },
+                  }
+                : undefined,
+        plugins: [
+            laravel({
+                input: [
+                    "resources/sass/app.scss",
+                    "resources/js/app.js"
+                ],
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+        ],
+        resolve: {
+            alias: {
+                vue: "vue/dist/vue.esm-bundler.js",
+            },
+        },
+    };
 });
