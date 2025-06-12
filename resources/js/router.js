d30de1c3e87be297ad606f946054f02eac6b05e1
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2025-06-12 21:39:51 +0300

    vue js files reset

--- resources/js/router.js
+++
@@ -1,9 +0,0 @@
-import { createMemoryHistory, createRouter } from "vue-router";
-import HomeVie from "./views/HomeView.vue";
-
-const router = createRouter({
-    history: createMemoryHistory(),
-    router: [{ path: "/", component: HomeVie }],
-});
-
-export default router;
