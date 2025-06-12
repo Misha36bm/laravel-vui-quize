ea27dd9b5dd0a9049225db1d96e3ea0a35daff32
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2025-06-12 21:35:24 +0300

    npm packages fixes

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,25 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
-export default defineConfig(() => {
-    const env = loadEnv(mode, process.cwd(), "");
-
-    return {
-        server:
-            env.APP_ENV === "local"
-                ? {
-                      host: "0.0.0.0",
-                      port: 5173,
-                      strictPort: true,
-                      hmr: {
-                          host: "quize.loc",
-                      },
-                  }
-                : undefined,
-        plugins: [
-            laravel({
-                input: [
-                    "resources/sass/app.scss",
-                    "resources/js/app.js"
-                ],
-                refresh: true,
-            }),
-            vue({
-                template: {
-                    transformAssetUrls: {
-                        base: null,
-                        includeAbsolute: false,
-                    },
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
                 },
-            }),
-        ],
-        resolve: {
-            alias: {
-                vue: "vue/dist/vue.esm-bundler.js",
             },
+        }),
+    ],
+    resolve: {
+        alias: {
+            vue: "vue/dist/vue.esm-bundler.js",
         },
-    };
+    },
 });
