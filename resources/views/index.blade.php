bd3da8c3b798061dcdd3e9ee8e996263ab318625
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2025-06-15 11:35:44 +0300

    add flyonui package for fast components style building

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Document</title>
 
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js', 'node_modules/flyonui/flyonui.js'])
 </head>
 
 <body id="app">
