a65b2492c649edd54742ced2adf90f5e42337d25
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2025-07-03 00:18:12 +0300

    rework nav bar component

--- resources/js/components/Base/NavbarComponent.vue
+++ resources/js/components/Base/NavbarComponent.vue
@@ -1,104 +1,92 @@
 <template>
-    <nav class="bg-gray-800">
-        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div class="relative flex h-16 items-center justify-between">
-                <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                    <!-- Mobile menu button-->
-                    <button type="button"
-                        class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset"
-                        aria-controls="mobile-menu" aria-expanded="false">
-                        <span class="absolute -inset-0.5"></span>
-                        <span class="sr-only">Open main menu</span>
-                        <!--
-            Icon when menu is closed.
-
-            Menu open: "hidden", Menu closed: "block"
-          -->
-                        <svg class="block size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" aria-hidden="true" data-slot="icon">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                        </svg>
-                        <!--
-            Icon when menu is open.
-
-            Menu open: "block", Menu closed: "hidden"
-          -->
-                        <svg class="hidden size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" aria-hidden="true" data-slot="icon">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                        </svg>
-                    </button>
-                </div>
-                <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                    <div class="flex shrink-0 items-center">
-                        <img class="h-8 w-auto"
-                            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                            alt="Your Company">
-                    </div>
-                    <div class="hidden sm:ml-6 sm:block">
-                        <div class="flex space-x-4">
-                            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                            <a href="#" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
-                                aria-current="page">Dashboard</a>
-                            <a href="#"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
-                            <a href="#"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
-                            <a href="#"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
-                        </div>
-                    </div>
-                </div>
-                <template v-if="user">
-
-                </template>
-                <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                    <!-- Profile dropdown -->
-                    <div class="relative ml-3">
-                        <div>
-                            <button type="button"
-                                class="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
-                                id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                <span class="absolute -inset-1.5"></span>
-                                <span class="sr-only">Open user menu</span>
-                                <img class="size-8 rounded-full"
-                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    alt="">
-                            </button>
-                        </div>
-
-                        <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5"
-                            role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                            <!-- Active: "bg-gray-100 outline-hidden", Not Active: "" -->
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                                id="user-menu-item-0">Your Profile</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                                id="user-menu-item-1">Settings</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                                id="user-menu-item-2">Sign out</a>
-                        </div>
-                    </div>
-                </div>
+    <div class="h-96 max-md:h-[31.25rem]">
+        <nav class="navbar rounded-box shadow-base-300/20 shadow-sm">
+            <div class="navbar-start">
+                <a class="link text-base-content link-neutral text-xl font-bold no-underline" href="#">
+                    Quize App
+                </a>
             </div>
-        </div>
-
-        <!-- Mobile menu, show/hide based on menu state. -->
-        <div class="sm:hidden" id="mobile-menu">
-            <div class="space-y-1 px-2 pt-2 pb-3">
-                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="#" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
-                    aria-current="page">Dashboard</a>
-                <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
-                <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
-                <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
+            <div class="navbar-center max-md:hidden">
+                <ul class="menu menu-horizontal gap-2 p-0 text-base rtl:ml-20">
+                    <!-- <li
+                        class="dropdown relative inline-flex [--auto-close:inside] [--offset:9] [--placement:bottom-end]">
+                        <button id="dropdown-end" type="button"
+                            class="dropdown-toggle dropdown-open:bg-base-content/10 dropdown-open:text-base-content max-md:px-2"
+                            aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
+                            Products
+                            <span class="icon-[tabler--chevron-down] dropdown-open:rotate-180 size-4"></span>
+                        </button>
+                        <ul class="dropdown-menu dropdown-open:opacity-100 hidden w-48" role="menu"
+                            aria-orientation="vertical" aria-labelledby="nested-dropdown">
+                            <li><a class="dropdown-item" href="#">Templates</a></li>
+                            <li><a class="dropdown-item" href="#">UI kits</a></li>
+                            <li class="dropdown relative [--auto-close:inside] [--offset:10] [--placement:right-start]">
+                                <button id="nested-dropdown-2"
+                                    class="dropdown-toggle dropdown-item dropdown-open:bg-base-content/10 dropdown-open:text-base-content justify-between"
+                                    aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
+                                    Components
+                                    <span class="icon-[tabler--chevron-right] size-4 rtl:rotate-180"></span>
+                                </button>
+                                <ul class="dropdown-menu dropdown-open:opacity-100 hidden w-48" role="menu"
+                                    aria-orientation="vertical" aria-labelledby="nested-dropdown-2">
+                                    <li><a class="dropdown-item" href="#">Basic</a></li>
+                                    <li>
+                                        <a class="dropdown-item" href="#">
+                                            Advanced
+                                            <span
+                                                class="badge badge-sm badge-soft badge-primary rounded-full">Pro</span>
+                                        </a>
+                                    </li>
+                                    <li
+                                        class="dropdown relative [--auto-close:inside] [--offset:10] rtl:[--placement:left-start] [--placement:right-start]">
+                                        <button id="nested-dropdown-2"
+                                            class="dropdown-toggle dropdown-item dropdown-open:bg-base-content/10 dropdown-open:text-base-content justify-between"
+                                            aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
+                                            Vendor
+                                            <span class="icon-[tabler--chevron-right] size-4 rtl:rotate-180"></span>
+                                        </button>
+                                        <ul class="dropdown-menu dropdown-open:opacity-100 hidden w-48" role="menu"
+                                            aria-orientation="vertical" aria-labelledby="nested-dropdown-2">
+                                            <li>
+                                                <a class="dropdown-item" href="#">
+                                                    Data tables
+                                                    <span
+                                                        class="badge badge-sm badge-soft badge-primary rounded-full">Pro</span>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a class="dropdown-item" href="#">
+                                                    Apex charts
+                                                    <span
+                                                        class="badge badge-sm badge-soft badge-primary rounded-full">Pro</span>
+                                                </a>
+                                            </li>
+                                            <li><a class="dropdown-item" href="#">Clipboard</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li> -->
+                    <li>
+                        <router-link :to="{ name: 'home' }">Home</router-link>
+                    </li>
+                    <li>
+                        <router-link :to="{ name: 'dashboard' }">Quizzes</router-link>
+                    </li>
+                    <li>
+                        <router-link :to="{ name: 'quizes.create' }">Create my own quize</router-link>
+                    </li>
+                </ul>
             </div>
-        </div>
-    </nav>
-
+            <div class="navbar-end items-center gap-4">
+                <!-- <a class="btn btn-primary" href="#">Login</a> -->
+                <router-link class="btn btn-primary" :to="{ name: 'login' }">
+                    Login
+                </router-link>
+            </div>
+        </nav>
+    </div>
 </template>
 
 <script>
